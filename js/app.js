e46c93be598e718015566465ee9e6bacabf8c38a
Aleksandr Ljamin <aleksandr.lmn@gmail.com>
2017-10-01 21:18:56 +1030

    made recomended changes - still missing the filter functionality

--- js/app.js
+++ js/app.js
@@ -1,34 +1,6 @@
 var map;
 var markers = [];
-var locations = [{
-  title:'Sazón', 
-  location: {lat:-34.924724, lng:138.600565},
-  foursquareID: '562594a0498efaedd938a30c'
-},{
-  title:'La Moka', 
-  location: {lat:-34.923602, lng:138.598170},
-  foursquareID: '53c738a1498e5a424a30802d'
-},{
-  title:'Aroma Cafe', 
-  location: {lat:-34.918830, lng:138.605131},
-  foursquareID: '4d9e9ec87f1b721ea17b8c0f'
-},{
-  title:'Gloria Jeans Coffees', 
-  location: {lat:-34.922878, lng:138.598870},
-  foursquareID: '4c044f7df56c2d7f64201e66'
-},{
-  title:'Handsome and the Duchess', 
-  location: {lat:-34.925173, lng:138.601431},
-  foursquareID: '5526fef1498e00d18e2d2818'
-},{
-  title:'Coffee Branch', 
-  location: {lat:-34.924165, lng:138.597753},
-  foursquareID: '4c086676009a0f476f26e6bf'
-},{
-  title:'Please Say Please', 
-  location: {lat:-34.924129, lng:138.601563},
-  foursquareID: '51367181e4b03619bc88cd6f'
-}];
+
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -73,10 +45,12 @@ function initMap() {
       self.setAnimation(google.maps.Animation.BOUNCE);
       setTimeout(function() {
           self.setAnimation(null);
-      }, 2000);
+      }, 1400);
     }
 
     map.fitBounds(bounds);
+
+    ko.applyBingings(new ViewModel());
 }
 
 
@@ -125,8 +99,16 @@ mapError = function() {
   alert("The Google Maps API didn't load correctly. Please try again later.");
 };
 
-var viewModel = function(){
+var Place = function (data){
   this.title = ko.observable(data.title);
+  this.address = ko.observable(data.address);
+  this.lat = ko.observable(data.lat);
+  this.lng = ko.observable(data.lng);
+  this.marker = ko.observable();
+};
+
+var ViewModel = function(){
+  
   this.filter = ko.observable('');
 
   //show the list of all the places
@@ -136,6 +118,5 @@ var viewModel = function(){
   this.showInfo = function(location) {
     google.maps.event.trigger(location.marker, 'click');
   };
-
-  ko.applyBingings(new viewModel());
+  
 };
---
+++ js/locations.js
@@ -0,0 +1,29 @@
+var locations = [{
+    title:'Sazón', 
+    location: {lat:-34.924724, lng:138.600565},
+    foursquareID: '562594a0498efaedd938a30c'
+  },{
+    title:'La Moka', 
+    location: {lat:-34.923602, lng:138.598170},
+    foursquareID: '53c738a1498e5a424a30802d'
+  },{
+    title:'Aroma Cafe', 
+    location: {lat:-34.918830, lng:138.605131},
+    foursquareID: '4d9e9ec87f1b721ea17b8c0f'
+  },{
+    title:'Gloria Jeans Coffees', 
+    location: {lat:-34.922878, lng:138.598870},
+    foursquareID: '4c044f7df56c2d7f64201e66'
+  },{
+    title:'Handsome and the Duchess', 
+    location: {lat:-34.925173, lng:138.601431},
+    foursquareID: '5526fef1498e00d18e2d2818'
+  },{
+    title:'Coffee Branch', 
+    location: {lat:-34.924165, lng:138.597753},
+    foursquareID: '4c086676009a0f476f26e6bf'
+  },{
+    title:'Please Say Please', 
+    location: {lat:-34.924129, lng:138.601563},
+    foursquareID: '51367181e4b03619bc88cd6f'
+  }];
